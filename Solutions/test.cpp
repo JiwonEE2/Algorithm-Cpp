373712cbba509eda3bd01337810283bbeb686d96
JiwonEE2 <jiwonchichi1@gmail.com>
2025-04-16 10:24:23 +0900

    세팅 및 테스트 코드 수정

--- Solutions/test.cpp
+++ Solutions/test.cpp
@@ -1,11 +1,25 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
-	int a;
-	cin >> a;
-	cout << a;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T, A, B;
+	vector<int> sum;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> A >> B;
+		sum.push_back(A + B);
+	}
+
+	for (int i = 0; i < T; i++)
+	{
+		cout << sum[i] << '\n';
+	}
 }
