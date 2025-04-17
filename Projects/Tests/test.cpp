02101d456877a35f4453349e82247e30d31731a1
JiwonEE2 <jiwonchichi1@gmail.com>
2025-04-17 21:32:43 +0900

    1406번 문제 확인해야 함

--- Projects/Tests/test.cpp
+++ Projects/Tests/test.cpp
@@ -1,7 +1,42 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
+
+const int MX = 600;
+char dat[MX];
+int pre[MX], nxt[MX];
+int unused = 1;
+
+void insert(int addr, char c)
+{
+	dat[unused] = c;
+	pre[unused] = addr;
+	nxt[unused] = nxt[addr];
+
+	if (nxt[addr] != -1)
+		pre[nxt[addr]] = unused;
+	nxt[addr] = unused;
+	unused++;
+}
+
+void erase(int addr)
+{
+	nxt[pre[addr]] = nxt[addr];
+	if (nxt[addr] != -1)
+		pre[nxt[addr]] = pre[addr];
+}
+
+void traverse()
+{
+	int cur = nxt[0];
+	while (cur != -1)
+	{
+		cout << dat[cur];
+		cur = nxt[cur];
+	}
+	cout << "\n\n";
+}
+
+int main(void)
 {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
@@ -9,17 +44,50 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int T, A, B;
-	vector<int> sum;
-	cin >> T;
-	for (int i = 0; i < T; i++)
+	fill(pre, pre + MX, -1);
+	fill(nxt, nxt + MX, -1);
+
+	string s;
+	int M;
+	cin >> s >> M;
+	for (int i = 0; i < s.length(); i++)
 	{
-		cin >> A >> B;
-		sum.push_back(A + B);
+		insert(i, s[i]);
 	}
 
-	for (int i = 0; i < T; i++)
+	char input;
+	int cursor = s.length();
+	int N = cursor;
+	while (M--)
 	{
-		cout << sum[i] << '\n';
+		cin >> input;
+		if (input == 'L' && cursor > 0)
+		{
+			cout << "[ L ]: 왼쪽으로 이동\n";
+			cursor--;
+		}
+		else if (input == 'D' && cursor < N)
+		{
+			cout << "[ D ]: 오른쪽으로 이동\n";
+			cursor++;
+		}
+		else if (input == 'B' && cursor != 0)
+		{
+			cout << "[ B ]: 왼쪽 삭제\n";
+			erase(cursor);
+			cursor--;
+			N--;
+		}
+		else if (input == 'P')
+		{
+			cin >> input;
+			cout << "[ P ]: 왼쪽에 추가\n";
+			insert(cursor, input);
+			cursor++;
+			N++;
+		}
+
+		cout << "cursor: " << cursor << '\n';
+		traverse();
 	}
 }
